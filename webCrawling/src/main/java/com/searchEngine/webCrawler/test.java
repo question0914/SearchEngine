5b3671bfc3625fbbe25784e8d4dfc03fd0163a63
question0914 <895820518@qq.com>
2017-09-18 16:11:05 -0700

    Write main

---
+++ webCrawling/src/main/java/com/searchEngine/webCrawler/test.java
@@ -0,0 +1,10 @@
+package com.searchEngine.webCrawler;
+
+/**
+ * Created by zijianli on 9/18/17.
+ */
+public class test {
+    public static void main(String args[]){
+        System.out.println("sss");
+    }
+}
