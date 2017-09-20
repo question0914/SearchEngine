daf7c8672ff97e80b5f41eb3f6bd95aeb978e1ab
question0914 <895820518@qq.com>
2017-09-20 14:53:46 -0700

    Updating test

--- webCrawling/src/main/java/com/searchEngine/webCrawler/test.java
+++ webCrawling/src/main/java/com/searchEngine/webCrawler/test.java
@@ -5,6 +5,6 @@ package com.searchEngine.webCrawler;
  */
 public class test {
     public static void main(String args[]){
-        System.out.println("sss");
+        System.out.println("xxx");
     }
 }
