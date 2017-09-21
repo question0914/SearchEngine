612ad3c0fcd7040fe53e33d73ca93131eed5962f
question0914 <895820518@qq.com>
2017-09-20 19:12:41 -0700

    Add controllers and related packages

---
+++ webCrawling/src/main/java/com/searchEngine/webCrawler/controller/Controller.java
@@ -0,0 +1,31 @@
+package com.searchEngine.webCrawler.controller;
+
+import edu.uci.ics.crawler4j.crawler.CrawlConfig;
+import edu.uci.ics.crawler4j.crawler.CrawlController;
+import edu.uci.ics.crawler4j.fetcher.PageFetcher;
+import edu.uci.ics.crawler4j.robotstxt.RobotstxtConfig;
+import edu.uci.ics.crawler4j.robotstxt.RobotstxtServer;
+
+/**
+ * Created by zijianli on 9/20/17.
+ */
+public class Controller {
+    public static void main(String[] args) throws Exception {
+        String crawlStorageFolder = "/data/crawl";
+        int numberOfCrawlers = 7;
+        CrawlConfig config = new CrawlConfig();
+        config.setCrawlStorageFolder(crawlStorageFolder);
+ /* Instantiate the controller for this crawl.*/
+        PageFetcher pageFetcher = new PageFetcher(config);
+        RobotstxtConfig robotstxtConfig = new RobotstxtConfig();
+        RobotstxtServer robotstxtServer = new RobotstxtServer(robotstxtConfig, pageFetcher);
+        CrawlController controller = new CrawlController(config, pageFetcher, robotstxtServer);
+ /* For each crawl, you need to add some seed urls. These are the first
+ * URLs that are fetched and then the crawler starts following links
+ * which are found in these pages */
+        controller.addSeed("http://www.cnn.com/");
+ /* Start the crawl. This is a blocking operation, meaning that your code
+ * will reach the line after this only when crawling is finished. */
+        controller.start(MyCrawler.class, numberOfCrawlers);
+    }
+}
